74fcc1265224bd898ddee3f4c29701ac8d20d62d
Om Patel <om31digital@gmail.com>
2025-01-08 16:20:04 -0500

    done

--- Web Development/JavaScript/JavaScript-1/Control Flow/Homework/script.js
+++ Web Development/JavaScript/JavaScript-1/Control Flow/Homework/script.js
@@ -45,4 +45,64 @@ else{
 }
 
 // #4
-let weather = "Snowy";
+weather = "sunny";
+switch (weather){
+    case "sunny":
+        console.log("Don't forget your sunglasses.");
+        break;
+    case "rainy":
+        console.log("Don't forget your umbrella.");
+        break;
+    case "cloudy":
+        console.log("Don't forget your coat.");
+        break;
+    default:
+        console.log("Enjoy your day. Hope the weather outside is good.")
+}
+
+// #5
+let i = 1;
+while (i<7){
+    console.log(i);
+    i++;
+}
+
+// #6
+for (i=5; i>0; i--){
+    console.log(i);
+}
+
+// #7
+i = 0
+do{
+    console.log(i);
+    i++;
+} while(i<4)
+
+// #8
+for (num=1; num<=15; num+=2){
+    console.log(num);
+}
+
+// #9
+for (let j=1; j<=6; j++) {
+    if (j===3) {
+        break;
+    }
+    console.log(j);
+}
+
+// #10
+let a = 1;
+while (a<=8){
+    if (a===5){
+        continue;
+    }
+    console.log(a);
+}
+
+// #11
+function calculateVolume(radius, height){
+    volume = radius*radius*height*Math.PI;
+    return volume
+}
