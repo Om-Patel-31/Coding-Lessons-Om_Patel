0780a843fb007333d0e28c48c3acca65ab97f84a
Om Patel <ompate538@ed.amdsb.ca>
2025-01-15 16:41:55 -0500

    feat: add HTML and JavaScript files for DOM modification example with dynamic content

---
+++ Web Development/JavaScript/JavaScript-1/Document Object Model/Examples/script.js
@@ -0,0 +1,4 @@
+document.body.style.backgroundColor = 'aqua';
+document.getElementById("name").innerText = 'Om Patel';
+document.getElementById("age").innerText = '19';
+document.getElementById("lastPara").innerText += ' it is fun';
