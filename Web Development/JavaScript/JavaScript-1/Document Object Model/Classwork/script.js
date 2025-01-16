0f23fcbd7bf02b96899a788bb37e7d15fa3cc8c2
Om Patel <ompate538@ed.amdsb.ca>
2025-01-16 16:19:21 -0500

    feat: enhance index.html and script.js with input handling and button click alert

--- Web Development/JavaScript/JavaScript-1/Document Object Model/Classwork/script.js
+++ Web Development/JavaScript/JavaScript-1/Document Object Model/Classwork/script.js
@@ -11,3 +11,13 @@ document.getElementById('container').appendChild(newParagraph);
 
 // #4
 document.getElementById('myPara').style.color = 'blue';
+
+// #5
+document.getElementById('myButton').addEventListener('click', function(){
+    alert('Button clicked!');
+});
+
+// #6
+document.querySelector("input").addEventListener("change", function() {
+    document.getElementById("myPara").innerText = document.querySelector('input').value;
+});
