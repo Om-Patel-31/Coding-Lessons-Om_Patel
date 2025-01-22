d01714dc648ad2a00a8ac381cb873464655ab89b
Om Patel <ompate538@ed.amdsb.ca>
2025-01-22 16:58:10 -0500

    feat: update script.js with additional ES6 examples and correct constant assignment error

--- Web Development/JavaScript/JavaScript-1/Introduction to ES6/Homework/script.js
+++ Web Development/JavaScript/JavaScript-1/Introduction to ES6/Homework/script.js
@@ -6,4 +6,22 @@ console.log(currentTemperature);
 // #2
 const pi = 3.14;
 console.log(pi);
-pi = 5;
+// pi = 5; TypeError: Assignment to constant variable.
+
+// #3
+const calculateArea = (length, width) => length*width;
+console.log(calculateArea);
+
+// #4
+let friendName = "Ben";
+let greetingMessage = `Hey ${friendName}!`;
+console.log(greetingMessage);
+
+// #5
+let day = "Wednesday";
+let month = "January";
+let year = 2025;
+let dateMessage = `Today is ${day} of ${month}, ${year}`;
+console.log(dateMessage);
+
+// #6
