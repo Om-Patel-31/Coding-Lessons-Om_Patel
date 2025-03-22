284b129833f0b02c419f8d2c59d963f70f2bce1a
Om Patel <ompate538@ed.amdsb.ca>
2025-03-22 17:08:48 -0400

    style: remove view toggle buttons and associated JavaScript functionality, update card layout for improved design

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('card-view-btn').addEventListener('click', function() {
-    document.querySelector('.content-cards').classList.add('card-view');
-    document.querySelector('.content-cards').classList.remove('list-view');
-});
-
-document.getElementById('list-view-btn').addEventListener('click', function() {
-    document.querySelector('.content-cards').classList.add('list-view');
-    document.querySelector('.content-cards').classList.remove('card-view');
-});
