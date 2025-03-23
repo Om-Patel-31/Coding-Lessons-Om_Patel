93fc56250b0da1f94bb6461cc76ca5459ac6f1df
Om Patel <ompate538@ed.amdsb.ca>
2025-03-23 16:51:12 -0400

    fix: update file paths in classes.js for correct code loading and remove unnecessary whitespace in classes.html

--- Website/javascript/python_js_files/classes.js
+++ Website/javascript/python_js_files/classes.js
@@ -15,7 +15,7 @@ function loadCode(filePath, elementId) {
         });
 }
 
-loadCode("/Python/Classes/classes.py", 'codeDisplay1');
-loadCode("/Python/Classes/Inheritance_and_Polymorphism.py", 'codeDisplay2');
-loadCode("/Python/Classes/'basta_fazoolin'.py", 'codeDisplay3');
-loadCode("/Python/Classes/Become_a_pokemon_master.py", 'codeDisplay4');
+loadCode("/Website/Python/Classes/classes.py", 'codeDisplay1');
+loadCode("/Website/Python/Classes/Inheritance_and_Polymorphism.py", 'codeDisplay2');
+loadCode("/Website/Python/Classes/basta_fazoolin.py", 'codeDisplay3');
+loadCode("/Website/Python/Classes/Become_a_pokemon_master.py", 'codeDisplay4');
