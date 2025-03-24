0ac5ede6bf345738282563a6924e0afe04c45812
Om Patel <ompate538@ed.amdsb.ca>
2025-03-24 19:53:47 -0400

    fix: correct file path for basta_fazoolin.py in classes.js and enhance code display formatting

--- Website/html/Python/Classes/classes.js
+++ Website/html/Python/Classes/classes.js
@@ -7,7 +7,9 @@ function loadCode(filePath, elementId) {
             return response.text();
         })
         .then(data => {
-            document.getElementById(elementId).innerHTML = `<code>${data}</code>`;
+            const lines = data.split("\n");
+            let formattedCode = lines.map(line => `<span>${line}</span>`).join("\n");
+            document.getElementById(elementId).innerHTML = formattedCode;
         })
         .catch(error => {
             console.error('Error loading the file:', error);
@@ -17,5 +19,5 @@ function loadCode(filePath, elementId) {
 
 loadCode("../../Python/Classes/classes.py", 'codeDisplay1');
 loadCode("../../Python/Classes/Inheritance_and_Polymorphism.py", 'codeDisplay2');
-loadCode("../../Python/Classes/basta_fazoolin'.py", 'codeDisplay3');
+loadCode("../../Python/Classes/basta_fazoolin.py", 'codeDisplay3');
 loadCode("../../Python/Classes/Become_a_pokemon_master.py", 'codeDisplay4');
